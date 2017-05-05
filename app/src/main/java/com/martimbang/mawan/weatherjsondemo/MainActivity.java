36f9309b77ea23046269eaceeda2b2fef1e8fa60
Samawi Samadikun <samawi.samadikun@gmail.com>
2017-05-05 23:29:16 +0700

    Added UI but still has fatal error bug

--- app/src/main/java/com/martimbang/mawan/weatherjsondemo/MainActivity.java
+++ app/src/main/java/com/martimbang/mawan/weatherjsondemo/MainActivity.java
@@ -1,22 +1,49 @@
 package com.martimbang.mawan.weatherjsondemo;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultTextView;
+    EditText cityEditText;
+
+    public void getWeather(View view) {
+        String content = "";
+        String cityName = "";
+
+        InputMethodManager methodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        methodManager.hideSoftInputFromWindow(cityEditText.getWindowToken(), 0);
+
+        try {
+
+            cityName = URLEncoder.encode(cityEditText.getText().toString(), "UTF-8");
+            DownloadTask task = new DownloadTask();
+            task.execute("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=58014140638c5a34d8b8a0e15e5c2ca8");
+
+        } catch (UnsupportedEncodingException e) {
+            Toast.makeText(getApplicationContext(), "Can not find weather", Toast.LENGTH_LONG);
+        }
+
+    }
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -47,10 +74,11 @@ public class MainActivity extends AppCompatActivity {
 
                 return result;
 
-            } catch (MalformedURLException e) {
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Can not find weather", Toast.LENGTH_LONG);
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Can not find weather", Toast.LENGTH_LONG);
             }
 
 
@@ -61,29 +89,43 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
 
+            String city = "";
+            String main = "";
+            String tem = "";
+            String hum  = "";
+            String textToDisplay = "";
+
             try {
 
                 JSONObject jsonObject = new JSONObject(result);
                 String weatherInfo = jsonObject.getString("weather");
                 String mainInfo = jsonObject.getString("main");
 
-                Log.i("city", jsonObject.getString("name"));
+                city = jsonObject.getString("name");
 
                 JSONArray weatherArray = new JSONArray(weatherInfo);
 
                 for (int i = 0; i < weatherArray.length(); i++) {
                     JSONObject jsonPart = weatherArray.getJSONObject(i);
-                    Log.i("main", jsonPart.getString("main"));
-                    Log.i("description", jsonPart.getString("description"));
+                    main = jsonPart.getString("main");
+//                    Log.i("main", jsonPart.getString("main"));
+//                    Log.i("description", jsonPart.getString("description"));
                 }
 
                 JSONObject temp = new JSONObject(mainInfo);
-                Log.i("temp", temp.getString("temp"));
-                Log.i("humidity", temp.getString("humidity"));
+
+                tem = temp.getString("temp");
+                hum = temp.getString("humidity");
+
+//                Log.i("temp", temp.getString("temp"));
+//                Log.i("humidity", temp.getString("humidity"));
+
+                textToDisplay = "Weather for " + city + "\r\n" + main + "\r\n" + "Temp: " + tem + "\r\n" + "Hum: " + hum;
+                resultTextView.setText(textToDisplay);
 
 
-            } catch (JSONException e) {
-                e.printStackTrace();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Can not find weather", Toast.LENGTH_LONG);
             }
 
         }
@@ -94,11 +136,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String content = "";
-
-        DownloadTask task = new DownloadTask();
-
-        task.execute("http://api.openweathermap.org/data/2.5/weather?q=Jakarta&units=metric&appid=58014140638c5a34d8b8a0e15e5c2ca8");
+        resultTextView = (TextView) findViewById(R.id.textView);
+        cityEditText = (EditText) findViewById(R.id.editText);
 
     }
 }
