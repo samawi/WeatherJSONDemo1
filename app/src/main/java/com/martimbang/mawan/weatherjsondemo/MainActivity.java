025d77cedadf8be5ca830921ab203a9300f25046
Samawi Samadikun <samawi.samadikun@gmail.com>
2017-05-05 22:26:46 +0700

    Second commit with standard download task and json parser

--- app/src/main/java/com/martimbang/mawan/weatherjsondemo/MainActivity.java
+++ app/src/main/java/com/martimbang/mawan/weatherjsondemo/MainActivity.java
@@ -1,15 +1,104 @@
 package com.martimbang.mawan.weatherjsondemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+
+                int data = reader.read();
+
+                while (data != -1) {
+
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+
+                }
+
+                return result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+
+                JSONObject jsonObject = new JSONObject(result);
+                String weatherInfo = jsonObject.getString("weather");
+                String mainInfo = jsonObject.getString("main");
+
+                Log.i("city", jsonObject.getString("name"));
+
+                JSONArray weatherArray = new JSONArray(weatherInfo);
+
+                for (int i = 0; i < weatherArray.length(); i++) {
+                    JSONObject jsonPart = weatherArray.getJSONObject(i);
+                    Log.i("main", jsonPart.getString("main"));
+                    Log.i("description", jsonPart.getString("description"));
+                }
+
+                JSONObject temp = new JSONObject(mainInfo);
+                Log.i("temp", temp.getString("temp"));
+                Log.i("humidity", temp.getString("humidity"));
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // your code goes here
+
+        String content = "";
+
+        DownloadTask task = new DownloadTask();
+
+        task.execute("http://api.openweathermap.org/data/2.5/weather?q=Jakarta&units=metric&appid=58014140638c5a34d8b8a0e15e5c2ca8");
+
     }
 }
